b060509694ba4f0f081af374c156c6620bd547fa
tgarvs <tgarvey12@gmail.com>
2025-08-20 16:08:55 +0100

    allowing for save states

--- two_input/Source/PluginProcessor.cpp
+++ two_input/Source/PluginProcessor.cpp
@@ -40,6 +40,7 @@ Two_inputAudioProcessor::Two_inputAudioProcessor()
     //Set intial model for pointer
 //    model_ptr = neuralNet9;
     
+    
 }
 
 Two_inputAudioProcessor::~Two_inputAudioProcessor()
@@ -228,15 +229,17 @@ juce::AudioProcessorEditor* Two_inputAudioProcessor::createEditor()
 //==============================================================================
 void Two_inputAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void Two_inputAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+        std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+        if (xmlState.get() != nullptr)
+            if (xmlState->hasTagName (apvts.state.getType()))
+                apvts.replaceState (juce::ValueTree::fromXml (*xmlState));
 }
 
 //==============================================================================
--- two_input/Source/PluginProcessor.h
+++ two_input/Source/PluginProcessor.h
@@ -62,6 +62,7 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 //    bool supportsDoublePrecisionProcessing() const override { return false; }
+    
 
 
     juce::AudioProcessorValueTreeState apvts;
